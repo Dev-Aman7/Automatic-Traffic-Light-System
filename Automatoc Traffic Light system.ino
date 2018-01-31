077d3482cb829a1214d600f984113a1006abd361
Rockzo77 <32014475+Rockzo77@users.noreply.github.com>
2018-01-31 18:00:33 +0530

    Add files via upload

---
+++ Automatoc Traffic Light system.ino
@@ -0,0 +1,55 @@
+int red=4;
+int yellow=3;
+int green=2;
+
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  int p=analogRead(A0);
+  int q=analogRead(A1);
+  int r=analogRead(A2);
+  int s=analogRead(A3);
+
+  
+  if ( p<500 &&  q<500 && r<500 && s<500)
+  {
+    digitalWrite(2,HIGH);
+    delay(1000000);
+    digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    delay(5000);
+    digitalWrite(4,HIGH);
+  }
+
+  else if( p<500 &&  q<500 && r<500 && s>500)
+  {
+    digitalWrite(2,HIGH);
+    delay(500000);
+    digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    delay(500);
+    digitalWrite(4,HIGH);
+  }
+  else if( p<500 &&  q<500 && r>500 && s<500)
+  {
+    digitalWrite(2,HIGH);
+    delay(25000);
+    digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    delay(500);
+    digitalWrite(4,HIGH);
+  }
+
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  // put your main code here, to run repeatedly:
+
+}
